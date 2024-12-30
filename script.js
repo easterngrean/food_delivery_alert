c6da7ef84c78b829d86323c1c3287f3144b1da55
easterngrean <42080907+easterngrean@users.noreply.github.com>
2024-12-30 12:36:32 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const circleAM = document.querySelector("#lightAM");
+const circlePM = document.querySelector("#lightPM");
+const btnAM = document.getElementById("btnAM");
+const btnPM = document.getElementById("btnPM");
 
+function lightChangeAM()
+{
+  circleAM.style.fill = "green";
+}
+
+function lightChangePM()
+{
+  circlePM.style.fill = "green";
+}
+
+btnAM.onclick = lightChangeAM;
+btnPM.onclick = lightChangePM;
